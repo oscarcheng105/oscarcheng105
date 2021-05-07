05c011c9e98da0a8c8169b9b72e6a4ae93d959c7
Oscar Cheng <oscarcheng105@gmail.com>
2021-05-07 05:59:56 -0500

    Delete CS97_Recipe directory

--- CS97_Recipe/client/src/components/tag/tagsArray.js
+++
@@ -1,4 +0,0 @@
-export const getTagsArray = ()=> ([
-    {id: 'African', name: "African"}, {id: 'Chinese', name: "Chinese"}, {id: 'American', name: "American"}, 
-    {id: 'German', name: "German"}, {id: 'Mexican', name: "Mexican"}
-])
